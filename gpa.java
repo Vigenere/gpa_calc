1754c70c41daf6ef68f9203a1837d84e063df858
Vigenere <Vigenere@jonathans-air.lan>
2014-01-19 15:31:39 -0800

    Second commit
    Added basic structure, and button actions
    
    To-do:
    Implement reGraph()
    Reposition components to create a smoother look

--- gpa.java
+++ gpa.java
@@ -3,15 +3,22 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
 import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Enumeration;
 
 public class gpa extends JFrame{
-	public static Stack<Integer[]> grades = new Stack<Integer[]>(); //Entries should be of the form {grade, units}
-	public static double[] weights = new double[13];
-	public static JPanel all, top, bottom, right, right_top, right_bottom, left, left_top, left_middle, left_bottom;
-	public static JButton add, undo, reset, update;
-	public static JRadioButton rb0, rb1, rb2, rb3, rb4, rb5, rb6, rb7, rb8, rb9, rb10, rb11, rb12;
-	public static JLabel gpa, add_class, grade, units, grade_dis;
-	public static JTextField tf0, tf1, tf2, tf3, tf4, tf5, tf6, tf7, tf8, tf9, tf10, tf11, tf12, unit_entry;
+	public Stack<Integer[]> grades = new Stack<Integer[]>(); //Entries should be of the form {grade, units}
+	public double[] weights = new double[13];
+	public JPanel all, top, bottom, right, right_top, right_bottom, left, left_top, left_middle, left_bottom; //for general structure
+	public JPanel jp0, jp1, jp2, jp3, jp4, jp5, jp6, jp7, jp8, jp9, jp10, jp11, jp12; //for right column
+	public JPanel grade_jp1, grade_jp2, grade_jp3, grade_jp4; //for radio buttons
+	public JPanel units_jp, buttons_jp, graph_jp;
+	public JButton add, undo, reset, update;
+	public JRadioButton rb0, rb1, rb2, rb3, rb4, rb5, rb6, rb7, rb8, rb9, rb10, rb11, rb12;
+	public JLabel gpa_l, add_class, grade_l, units, grade_dis, weights_l;
+	public JTextField tf0, tf1, tf2, tf3, tf4, tf5, tf6, tf7, tf8, tf9, tf10, tf11, tf12, unit_entry;
+	public ButtonGroup rb_bg;
+	public GraphDrawer gradedis_graph;
 
 	public gpa(int width, int height) {
 		setDefaultWeights();
@@ -27,9 +34,9 @@ public class gpa extends JFrame{
 		setVisible(true);
 	}
 
-	public class ExitButtonHandler implements ActionListener{
+	public class UpdateWeightsHandler implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
-			System.exit(0);
+			updateWeights();
 		}
 	}
 
@@ -51,10 +58,14 @@ public class gpa extends JFrame{
 		}
 	}
 
-	public static void main(String[] args) {
-		gpa win = new gpa(600,800);
+	public class GraphDrawer extends JPanel{
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.fillRect(50, 0, 100, 100);
+		}
 	}
 
+	//----------------Structure----------------
 	/*Sets default weights according to UC Berkeley's grading scale
 	weights[0] = 0.0 (F)
 	weights[1] = 0.7 (D-)
@@ -63,7 +74,7 @@ public class gpa extends JFrame{
 	weights[11] = 4.0 (A)
 	weights[12] = 4.0 (A+)
 	*/
-	public static void setDefaultWeights() {
+	public void setDefaultWeights() {
 		weights[0] = 0.0; //F
 		weights[1] = 0.7; //D-
 		weights[2] = 1.0; //D
@@ -79,7 +90,7 @@ public class gpa extends JFrame{
 		weights[12] = 4.0; //A
 	}
 
-	public static void addJPanels() {
+	private void addJPanels() {
 		all = new JPanel();
 		bottom = new JPanel();
 		top = new JPanel();
@@ -95,16 +106,19 @@ public class gpa extends JFrame{
 		top.setLayout(new BoxLayout(top, BoxLayout.X_AXIS));
 		bottom.setLayout(new BoxLayout(bottom, BoxLayout.X_AXIS));
 		right.setLayout(new BoxLayout(right, BoxLayout.Y_AXIS));
+		right_top.setLayout(new BoxLayout(right_top, BoxLayout.Y_AXIS));
 		left.setLayout(new BoxLayout(left, BoxLayout.Y_AXIS));
+		left_middle.setLayout(new BoxLayout(left_middle, BoxLayout.Y_AXIS));
+		left_bottom.setLayout(new BoxLayout(left_bottom, BoxLayout.Y_AXIS));
 
 		all.setPreferredSize(new Dimension(600, 800));
 		top.setPreferredSize(new Dimension(600, 50));
 		bottom.setPreferredSize(new Dimension(600, 750));
 		right.setPreferredSize(new Dimension(200,750));
 		left.setPreferredSize(new Dimension(400, 750));
-		left_top.setPreferredSize(new Dimension(400,100));
+		left_top.setPreferredSize(new Dimension(400,50));
 		left_middle.setPreferredSize(new Dimension(400,400));
-		left_bottom.setPreferredSize(new Dimension(400, 250));
+		left_bottom.setPreferredSize(new Dimension(400, 350));
 		right_top.setPreferredSize(new Dimension(200, 700));
 		right_bottom.setPreferredSize(new Dimension(200, 50));
 
@@ -115,8 +129,8 @@ public class gpa extends JFrame{
 		right_bottom.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
 		left_top.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
 		left_middle.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
-		left_bottom.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
-		*/
+		*/left_bottom.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+		
 
 		all.add(top);
 		all.add(bottom);
@@ -129,55 +143,271 @@ public class gpa extends JFrame{
 		left.add(left_bottom);
 	}
 
-	public static void addComponents() {
+	private void addComponents() {
 		add = new JButton("Add");
 		undo = new JButton("Undo");
 		reset = new JButton("Reset");
 		update = new JButton("Update");
-		rb0 = new JRadioButton();
-		rb1 = new JRadioButton();
-		rb2 = new JRadioButton();
-		rb3 = new JRadioButton();
-		rb4 = new JRadioButton();
-		rb5 = new JRadioButton();
-		rb6 = new JRadioButton();
-		rb7 = new JRadioButton();
-		rb8 = new JRadioButton();
-		rb9 = new JRadioButton();
-		rb10 = new JRadioButton();
-		rb11 = new JRadioButton();
-		rb12 = new JRadioButton();
-		gpa = new JLabel("GPA");
+		rb0 = new JRadioButton("F");
+		rb1 = new JRadioButton("D-");
+		rb2 = new JRadioButton("D");
+		rb3 = new JRadioButton("D+");
+		rb4 = new JRadioButton("C-");
+		rb5 = new JRadioButton("C");
+		rb6 = new JRadioButton("C+");
+		rb7 = new JRadioButton("B-");
+		rb8 = new JRadioButton("B");
+		rb9 = new JRadioButton("B+");
+		rb10 = new JRadioButton("A-");
+		rb11 = new JRadioButton("A");
+		rb12 = new JRadioButton("A+");
+		gpa_l = new JLabel("GPA:");
 		add_class = new JLabel("Add Class");
-		grade = new JLabel("Grade");
+		grade_l = new JLabel("Grade");
 		units = new JLabel("Units");
 		grade_dis = new JLabel("Grade Distribution");
-		tf0 = new JTextField();
-		tf1 = new JTextField();
-		tf2 = new JTextField();
-		tf3 = new JTextField();
-		tf4 = new JTextField();
-		tf5 = new JTextField();
-		tf6 = new JTextField();
-		tf7 = new JTextField();
-		tf8 = new JTextField();
-		tf9 = new JTextField();
-		tf10 = new JTextField();
-		tf11 = new JTextField();
-		tf12 = new JTextField();
+		weights_l = new JLabel("Weights");
+		tf0 = new JTextField("0.0");
+		tf1 = new JTextField("0.7");
+		tf2 = new JTextField("1");
+		tf3 = new JTextField("1.3");
+		tf4 = new JTextField("1.7");
+		tf5 = new JTextField("2");
+		tf6 = new JTextField("2.3");
+		tf7 = new JTextField("2.7");
+		tf8 = new JTextField("3");
+		tf9 = new JTextField("3.3");
+		tf10 = new JTextField("3.7");
+		tf11 = new JTextField("4");
+		tf12 = new JTextField("4");
 		unit_entry = new JTextField();
+
+		add.addActionListener(new AddClassHandler());
+		undo.addActionListener(new UndoClassHandler());
+		reset.addActionListener(new ResetClassHandler());
+		update.addActionListener(new UpdateWeightsHandler());
+
+		top.add(gpa_l);
+
+		right_top.add(weights_l);
+		addRightComponents();
+		addLeftComponents();
 	}
 
-	private void addClass() {
+	private void addRightComponents() {
+		jp12 = new JPanel();
+		jp12.setLayout(new BoxLayout(jp12, BoxLayout.X_AXIS));
+		jp12.add(new JLabel("A+ "));
+		jp12.add(tf12);
+		right_top.add(jp12);
+		jp11 = new JPanel();
+		jp11.setLayout(new BoxLayout(jp11, BoxLayout.X_AXIS));
+		jp11.add(new JLabel("A  "));
+		jp11.add(tf11);
+		right_top.add(jp11);
+		jp10 = new JPanel();
+		jp10.setLayout(new BoxLayout(jp10, BoxLayout.X_AXIS));
+		jp10.add(new JLabel("A- "));
+		jp10.add(tf10);
+		right_top.add(jp10);
+		jp9 = new JPanel();
+		jp9.setLayout(new BoxLayout(jp9, BoxLayout.X_AXIS));
+		jp9.add(new JLabel("B+ "));
+		jp9.add(tf9);
+		right_top.add(jp9);
+		jp8 = new JPanel();
+		jp8.setLayout(new BoxLayout(jp8, BoxLayout.X_AXIS));
+		jp8.add(new JLabel("B  "));
+		jp8.add(tf8);
+		right_top.add(jp8);
+		jp7 = new JPanel();
+		jp7.setLayout(new BoxLayout(jp7, BoxLayout.X_AXIS));
+		jp7.add(new JLabel("B- "));
+		jp7.add(tf7);
+		right_top.add(jp7);
+		jp6 = new JPanel();
+		jp6.setLayout(new BoxLayout(jp6, BoxLayout.X_AXIS));
+		jp6.add(new JLabel("C+ "));
+		jp6.add(tf6);
+		right_top.add(jp6);
+		jp5 = new JPanel();
+		jp5.setLayout(new BoxLayout(jp5, BoxLayout.X_AXIS));
+		jp5.add(new JLabel("C "));
+		jp5.add(tf5);
+		right_top.add(jp5);
+		jp4 = new JPanel();
+		jp4.setLayout(new BoxLayout(jp4, BoxLayout.X_AXIS));
+		jp4.add(new JLabel("C- "));
+		jp4.add(tf4);
+		right_top.add(jp4);
+		jp3 = new JPanel();
+		jp3.setLayout(new BoxLayout(jp3, BoxLayout.X_AXIS));
+		jp3.add(new JLabel("D+ "));
+		jp3.add(tf3);
+		right_top.add(jp3);
+		jp2 = new JPanel();
+		jp2.setLayout(new BoxLayout(jp2, BoxLayout.X_AXIS));
+		jp2.add(new JLabel("D  "));
+		jp2.add(tf2);
+		right_top.add(jp2);
+		jp1 = new JPanel();
+		jp1.setLayout(new BoxLayout(jp1, BoxLayout.X_AXIS));
+		jp1.add(new JLabel("D- "));
+		jp1.add(tf1);
+		right_top.add(jp1);
+		jp0 = new JPanel();
+		jp0.setLayout(new BoxLayout(jp0, BoxLayout.X_AXIS));
+		jp0.add(new JLabel("F  "));
+		jp0.add(tf0);
+		right_top.add(jp0);
 
+		right_bottom.add(update);
+	}
+
+	private void addLeftComponents() {
+		left_top.add(add_class);
+		left_middle.add(grade_l);
+
+		rb_bg = new ButtonGroup();
+		rb_bg.add(rb12);
+		rb_bg.add(rb11);
+		rb_bg.add(rb10);
+		rb_bg.add(rb9);
+		rb_bg.add(rb8);
+		rb_bg.add(rb7);
+		rb_bg.add(rb6);
+		rb_bg.add(rb5);
+		rb_bg.add(rb4);
+		rb_bg.add(rb3);
+		rb_bg.add(rb2);
+		rb_bg.add(rb1);
+		rb_bg.add(rb0);
+
+		grade_jp1 = new JPanel();
+		grade_jp1.setLayout(new BoxLayout(grade_jp1, BoxLayout.X_AXIS));
+		grade_jp2 = new JPanel();
+		grade_jp2.setLayout(new BoxLayout(grade_jp2, BoxLayout.X_AXIS));
+		grade_jp3 = new JPanel();
+		grade_jp3.setLayout(new BoxLayout(grade_jp3, BoxLayout.X_AXIS));
+		grade_jp4 = new JPanel();
+		grade_jp4.setLayout(new BoxLayout(grade_jp4, BoxLayout.X_AXIS));
+		grade_jp1.add(rb12);
+		grade_jp1.add(rb11);
+		grade_jp1.add(rb10);
+		grade_jp2.add(rb9);
+		grade_jp2.add(rb8);
+		grade_jp2.add(rb7);
+		grade_jp3.add(rb6);	
+		grade_jp3.add(rb5);		
+		grade_jp3.add(rb4);		
+		grade_jp4.add(rb3);	
+		grade_jp4.add(rb2);		
+		grade_jp4.add(rb1);	
+		grade_jp4.add(rb0);
+
+		left_middle.add(grade_jp1);
+		left_middle.add(grade_jp2);
+		left_middle.add(grade_jp3);
+		left_middle.add(grade_jp4);
+
+		units_jp = new JPanel();
+		units_jp.setLayout(new BoxLayout(units_jp, BoxLayout.X_AXIS));
+		units_jp.add(units);
+		units_jp.add(unit_entry);
+		left_middle.add(units_jp);
+
+		buttons_jp = new JPanel();
+		buttons_jp.setLayout(new BoxLayout(buttons_jp, BoxLayout.X_AXIS));
+		buttons_jp.add(add);
+		buttons_jp.add(undo);
+		buttons_jp.add(reset);
+		left_middle.add(buttons_jp);
+
+		gradedis_graph = new GraphDrawer();
+		left_bottom.add(grade_dis);
+		left_bottom.add(gradedis_graph);
+	}
+
+	//----------------Actions----------------
+
+	private void addClass() {
+		int grade = 0;
+		int units = 0;
+		boolean selected = false;
+		Enumeration<AbstractButton> jrb_enum = rb_bg.getElements();
+		while (jrb_enum.hasMoreElements()) {
+			AbstractButton jrb = jrb_enum.nextElement();
+
+			if (jrb.isSelected()) {
+				switch (jrb.getText()) {
+					case "A+":
+						grade = 12;
+						break;
+					case "A":
+						grade = 11;
+						break;
+					case "A-":
+						grade = 10;
+						break;
+					case "B+":
+						grade = 9;
+						break;
+					case "B":
+						grade = 8;
+						break;
+					case "B-":
+						grade = 7;
+						break;
+					case "C+":
+						grade = 6;
+						break;
+					case "C":
+						grade = 5;
+						break;
+					case "C-":
+						grade = 4;
+						break;
+					case "D+":
+						grade = 3;
+						break;
+					case "D":
+						grade = 2;
+						break;
+					case "D-":
+						grade = 1;
+						break;
+					case "F":
+						grade = 0;
+						break;
+				}
+
+				selected = true;
+			}
+		}
+		if (!selected) {
+			JOptionPane.showMessageDialog(null, "No grade selected", "Error", JOptionPane.ERROR_MESSAGE);
+			return;				
+		}
+		try {
+			units = Integer.parseInt(unit_entry.getText());
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Units must be an integer", "Error", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+
+		Integer[] grade_unit = {grade, units};
+		grades.push(grade_unit);
+		updateGrades();
 	}
 
 	private void undoClass() {
-
+		grades.pop();
+		updateGrades();
 	}
 
 	private void resetClass() {
-
+		grades = new Stack<Integer[]>();
+		updateGrades();
 	}
 
 	private void reGraph() {
@@ -185,7 +415,29 @@ public class gpa extends JFrame{
 	}
 
 	private void updateWeights() {
+		try {
+			weights[0] = Double.parseDouble(tf0.getText());
+			weights[1] = Double.parseDouble(tf1.getText());
+			weights[2] = Double.parseDouble(tf2.getText());
+			weights[3] = Double.parseDouble(tf3.getText());
+			weights[4] = Double.parseDouble(tf4.getText());
+			weights[5] = Double.parseDouble(tf5.getText());
+			weights[6] = Double.parseDouble(tf6.getText());
+			weights[7] = Double.parseDouble(tf7.getText());
+			weights[8] = Double.parseDouble(tf8.getText());
+			weights[9] = Double.parseDouble(tf9.getText());
+			weights[10] = Double.parseDouble(tf10.getText());
+			weights[11] = Double.parseDouble(tf11.getText());
+			weights[12] = Double.parseDouble(tf12.getText());
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Invalid input", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+	}
 
+	private void updateGrades() {
+		double gpa_calc = computeGPA();
+		gpa_l.setText("GPA: " + gpa_calc);
+		reGraph();
 	}
 
 	/*
@@ -196,9 +448,13 @@ public class gpa extends JFrame{
 		double units = 0;
 		for (int n = 0; n < grades.size(); n++) {
 			Integer[] grade = grades.elementAt(n);
-			sum += weights[grade[0]];
+			sum += weights[grade[0]] * grade[1];
 			units += (double)grade[1];
 		}
 		return sum/units;
+	}
+
+	public static void main(String[] args) {
+		gpa win = new gpa(600,800);
 	}
 }
